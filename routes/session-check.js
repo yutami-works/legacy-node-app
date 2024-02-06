bc5bce10e42fc4354b3e48da84b56301d4e8aa78
yutami <sabacan623@gmail.com>
2024-02-06 22:41:50 +0900

    session check middleware

--- routes/session-check.js
+++ routes/session-check.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  if (!req.session.user) {
+    //res.render('timeout);
+    res.redirect('login');
+  } else {
+    next();
+  }
 });
 
 module.exports = router;
